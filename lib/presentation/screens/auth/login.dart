eff0b374041405d955ae9b68236e59ec71158cea
Mustafa <mm0118250@gmail.com>
2025-11-09 01:09:11 +0200

    refactor code and add validation to custom text form field

--- lib/presentation/screens/auth/login.dart
+++ lib/presentation/screens/auth/login.dart
@@ -1,16 +1,26 @@
 import 'package:dental_lab_app/core/constants/app_constants.dart';
 import 'package:dental_lab_app/core/routing/app_router.dart';
 import 'package:dental_lab_app/core/theme/app_colors.dart';
+import 'package:dental_lab_app/presentation/screens/auth/widgets/custom_txt.dart';
 import 'package:dental_lab_app/presentation/widgets/custom_btn.dart';
 import 'package:dental_lab_app/presentation/widgets/custom_text_field.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
-class Login extends StatelessWidget {
+class Login extends StatefulWidget {
   const Login({super.key});
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  bool isPasswordVisible = false;
   @override
   Widget build(BuildContext context) {
-    GlobalKey<FormState> formKey = GlobalKey<FormState>();
     return Scaffold(
       body: SafeArea(
         child: SizedBox.expand(
@@ -25,64 +35,59 @@ class Login extends StatelessWidget {
                     const SizedBox(height: 70),
                     SvgPicture.asset(AppImages.logo),
                     const SizedBox(height: 15),
-                    Text(
-                      AppStrings.welcomeBack,
-                      style: TextStyle(
-                        color: AppColors.whiteColor,
-                        fontSize: 28,
-                        fontWeight: FontWeight.w700,
-                      ),
+                    CustomText(
+                      txt: AppStrings.welcomeBack,
+                      color: AppColors.whiteColor,
+                      fontWeight: FontWeight.w700,
+                      fontSize: 28,
                     ),
                     const SizedBox(height: 20),
-                    Text(
-                      AppStrings.loginToYourAccount,
-                      style: TextStyle(
-                        color: AppColors.whiteColor70,
-                        fontSize: 16,
-                        fontWeight: FontWeight.w400,
-                      ),
+                    CustomText(
+                      txt: AppStrings.loginToYourAccount,
+                      color: AppColors.whiteColor70,
+                      fontWeight: FontWeight.w400,
                     ),
                     const SizedBox(height: 20),
                     Row(
                       children: [
-                        Text(
-                          AppStrings.email,
-                          style: TextStyle(
-                            color: AppColors.whiteColor,
-                            fontSize: 16,
-                            fontWeight: FontWeight.w500,
-                          ),
+                        CustomText(
+                          txt: AppStrings.email,
+                          color: AppColors.whiteColor,
+                          fontWeight: FontWeight.w500,
                         ),
                       ],
                     ),
                     const SizedBox(height: 16),
                     CustomTextField(
+                      controller: emailController,
                       hint: AppStrings.enterYourEmail,
-                      suffixIcon: IconButton(
-                        onPressed: () {},
-                        icon: Icon(Icons.email, color: AppColors.greyColor),
-                      ),
+                      suffixIcon: Icon(Icons.email, color: AppColors.greyColor),
                     ),
                     const SizedBox(height: 16),
                     Row(
                       children: [
-                        Text(
-                          AppStrings.password,
-                          style: TextStyle(
-                            color: AppColors.whiteColor,
-                            fontSize: 16,
-                            fontWeight: FontWeight.w500,
-                          ),
+                        CustomText(
+                          txt: AppStrings.password,
+                          color: AppColors.whiteColor,
+                          fontWeight: FontWeight.w500,
                         ),
                       ],
                     ),
                     const SizedBox(height: 16),
                     CustomTextField(
+                      controller: passwordController,
+                      obscureText: !isPasswordVisible,
                       hint: AppStrings.enterYourPassword,
                       suffixIcon: IconButton(
-                        onPressed: () {},
+                        onPressed: () {
+                          setState(() {
+                            isPasswordVisible = !isPasswordVisible;
+                          });
+                        },
                         icon: Icon(
-                          Icons.visibility_off,
+                          isPasswordVisible
+                              ? Icons.visibility
+                              : Icons.visibility_off,
                           color: AppColors.greyColor,
                         ),
                       ),
@@ -91,27 +96,28 @@ class Login extends StatelessWidget {
                     Row(
                       mainAxisAlignment: MainAxisAlignment.end,
                       children: [
-                        Text(
-                          AppStrings.forgotPassword,
-                          style: TextStyle(
-                            color: AppColors.yellowColor,
-                            fontSize: 14,
-                            fontWeight: FontWeight.w400,
-                          ),
+                        CustomText(
+                          txt: AppStrings.forgotPassword,
+                          color: AppColors.yellowColor,
+                          fontWeight: FontWeight.w500,
                         ),
                       ],
                     ),
                     const SizedBox(height: 30),
                     CustomBtn(
-                      onTap: () {},
+                      onTap: () {
+                        if (formKey.currentState!.validate()) {
+                          Navigator.pushNamed(
+                            context,
+                            Routes.registerRoute,
+                          );
+                        }
+                      },
                       child: Center(
-                        child: Text(
-                          AppStrings.login,
-                          style: TextStyle(
-                            color: AppColors.blackColor,
-                            fontSize: 18,
-                            fontWeight: FontWeight.w600,
-                          ),
+                        child: CustomText(
+                          txt: AppStrings.login,
+                          color: AppColors.blackColor,
+                          fontWeight: FontWeight.w500,
                         ),
                       ),
                     ),
@@ -129,8 +135,10 @@ class Login extends StatelessWidget {
                             AppStrings.signUp,
                             style: TextStyle(color: AppColors.yellowColor),
                           ),
-                          onTap: () =>
-                              Navigator.pushNamed(context, Routes.registerRoute),
+                          onTap: () => Navigator.pushNamed(
+                            context,
+                            Routes.registerRoute,
+                          ),
                         ),
                       ],
                     ),
